2fe075d5a511f007e369218b63b7732ee54b3303
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-30 19:36:58 +0300

    master: doc generator.

--- appImpl/src/main/kotlin/ru/alex3koval/docGenerator/appImpl/service/DocumentGeneratorImpl.kt
+++ appImpl/src/main/kotlin/ru/alex3koval/docGenerator/appImpl/service/DocumentGeneratorImpl.kt
@@ -5,8 +5,8 @@ import ru.alex3koval.docGenerator.domain.contract.BaseGeneratedDocDTO
 import ru.alex3koval.docGenerator.domain.contract.Document
 import ru.alex3koval.docGenerator.domain.contract.DocumentGenerator
 
-class DocumentGeneratorImpl<R, T : BaseGeneratedDocDTO<R>> : DocumentGenerator<R, T> {
-    override fun generate(data: T): Mono<Document> {
+class DocumentGeneratorImpl : DocumentGenerator {
+    override fun <DTO : BaseGeneratedDocDTO> generate(data: DTO): Mono<Document> {
         TODO("Not yet implemented")
     }
 }
--- domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/contract/DocumentGenerator.kt
+++ domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/contract/DocumentGenerator.kt
@@ -2,6 +2,6 @@ package ru.alex3koval.docGenerator.domain.contract
 
 import reactor.core.publisher.Mono
 
-interface DocumentGenerator<R, T: BaseGeneratedDocDTO<R>> {
-    fun generate(data: T): Mono<Document>
+interface DocumentGenerator {
+    fun <DTO : BaseGeneratedDocDTO> generate(data: DTO): Mono<Document>
 }
