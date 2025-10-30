4a88ee16feb5ccd63d930d3917fafcfef6912cff
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-30 19:36:19 +0300

    master: not needed now.

--- domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/contract/BaseGeneratedDocDTO.kt
+++ domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/contract/BaseGeneratedDocDTO.kt
@@ -1,8 +1,3 @@
 package ru.alex3koval.docGenerator.domain.contract
 
-import ru.alex3koval.docGenerator.domain.vo.DocumentFormat
-
-abstract class BaseGeneratedDocDTO<T> {
-    abstract val templateId: T
-    abstract val format: DocumentFormat
-}
+abstract class BaseGeneratedDocDTO
