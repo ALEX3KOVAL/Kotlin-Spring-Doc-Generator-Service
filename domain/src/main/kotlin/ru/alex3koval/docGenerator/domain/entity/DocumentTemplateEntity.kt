e5106be1e5c39721c2da7cd29817f2c2444b0cae
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-30 19:36:49 +0300

    master: entities.

--- domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/entity/DocumentTemplateEntity.kt
+++ domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/entity/DocumentTemplateEntity.kt
@@ -3,8 +3,9 @@ package ru.alex3koval.docGenerator.domain.entity
 import ru.alex3koval.docGenerator.domain.contract.BaseGeneratedDocDTO
 import ru.alex3koval.docGenerator.domain.vo.DocumentFormat
 
-class DocumentEntity<ID, FILE_ID> internal constructor(
+class DocumentTemplateEntity<ID, FILE_ID> internal constructor(
     val id: ID,
+    val clazz: String,
     val fileId: FILE_ID,
     val domainDto: BaseGeneratedDocDTO,
     val format: DocumentFormat
--- storage/src/main/kotlin/ru/alex3koval/docGenerator/storage/entity/DocumentTemplate.kt
+++ storage/src/main/kotlin/ru/alex3koval/docGenerator/storage/entity/DocumentTemplate.kt
@@ -6,18 +6,16 @@ import org.springframework.data.relational.core.mapping.Table
 import ru.alex3koval.docGenerator.domain.vo.DocumentFormat
 import java.time.LocalDateTime
 
-@Table(name = "document")
-data class Document<ID : Any, FILE_ID, TEMPLATE_ID>(
+@Table(name = "document_template")
+data class DocumentTemplate<ID : Any, FILE_ID>(
     @Id
     val id: ID? = null,
-    @Column("file_id")
-    val fileId: FILE_ID,
-    @Column("template_id")
-    val templateId: TEMPLATE_ID,
     @Column("model")
     val jsonModel: String,
     @Column("format")
     val format: DocumentFormat,
+    @Column("file_id")
+    val fileId: FILE_ID,
     @Column("clazz")
     val clazz: String,
     @Column("created_at")
