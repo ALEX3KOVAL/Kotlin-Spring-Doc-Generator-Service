b81463029d77ef0b376d29499c8e28596d98aab7
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-30 19:37:51 +0300

    master: config's.

--- configuration/src/main/kotlin/ru/alex3koval/docGenerator/configuration/di/TypeDependentConfiguration.kt
+++ configuration/src/main/kotlin/ru/alex3koval/docGenerator/configuration/di/TypeDependentConfiguration.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.web.reactive.function.client.WebClient
 import ru.alex3koval.docGenerator.appImpl.command.factory.CreateDocumentCommandFactory
 import ru.alex3koval.docGenerator.appImpl.service.DocumentGeneratorImpl
@@ -11,42 +12,60 @@ import ru.alex3koval.docGenerator.appImpl.service.DocumentServiceImpl
 import ru.alex3koval.docGenerator.appImpl.service.FileServiceFacadeImpl
 import ru.alex3koval.docGenerator.domain.contract.DocumentGenerator
 import ru.alex3koval.docGenerator.domain.repository.document.DocumentRepository
+import ru.alex3koval.docGenerator.domain.repository.document.DocumentTemplateRepository
 import ru.alex3koval.docGenerator.domain.service.DocumentService
 import ru.alex3koval.docGenerator.domain.service.FileServiceFacade
 import ru.alex3koval.docGenerator.storage.repository.impl.document.DocumentRepositoryImpl
+import ru.alex3koval.docGenerator.storage.repository.impl.document.DocumentTemplateRepositoryImpl
 import ru.alex3koval.docGenerator.storage.repository.orm.OrmDocumentRepository
+import ru.alex3koval.docGenerator.storage.repository.orm.OrmDocumentTemplateRepository
 
 @Configuration
 class TypeDependentConfiguration(
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val template: R2dbcEntityTemplate
 ) {
     @Bean
-    fun documentGenerator(): DocumentGenerator<ULong, *> = DocumentGeneratorImpl()
+    fun documentGenerator(): DocumentGenerator = DocumentGeneratorImpl()
 
     @Bean
     fun documentRepository(
-        ormDocumentRepository: OrmDocumentRepository<ULong, ULong>
-    ): DocumentRepository<ULong, ULong> = DocumentRepositoryImpl(
-        ormRepository = ormDocumentRepository
+        ormDocumentRepository: OrmDocumentRepository<ULong, ULong, ULong>
+    ): DocumentRepository<ULong, ULong, ULong> = DocumentRepositoryImpl(
+        ormRepository = ormDocumentRepository,
+        objectMapper = objectMapper,
+        template = template
+    )
+
+    @Bean
+    fun documentTemplateRepository(
+        ormDocumentTemplateRepository: OrmDocumentTemplateRepository<ULong, ULong>,
+    ): DocumentTemplateRepository<ULong, ULong> = DocumentTemplateRepositoryImpl(
+        ormRepository = ormDocumentTemplateRepository,
+        objectMapper = objectMapper,
+        template = template
     )
 
     @Bean
     fun documentService(
-        documentRepository: DocumentRepository<ULong, ULong>
-    ): DocumentService<ULong, ULong> = DocumentServiceImpl(
-        repository = documentRepository,
+        documentRepository: DocumentRepository<ULong, ULong, ULong>,
+        documentTemplateRepository: DocumentTemplateRepository<ULong, ULong>
+    ): DocumentService<ULong, ULong, ULong> = DocumentServiceImpl(
+        documentRepository = documentRepository,
+        documentTemplateRepository = documentTemplateRepository,
         objectMapper = objectMapper
     )
 
     @Bean
     fun createDocumentCommandFactory(
-        documentService: DocumentService<ULong, ULong>,
-        documentGenerator: DocumentGenerator<ULong, *>,
+        documentService: DocumentService<ULong, ULong, ULong>,
+        documentGenerator: DocumentGenerator,
         fileServiceFacade: FileServiceFacade<ULong>
-    ): CreateDocumentCommandFactory<ULong, ULong, *> = CreateDocumentCommandFactory(
+    ): CreateDocumentCommandFactory<ULong, ULong, ULong> = CreateDocumentCommandFactory(
         documentService = documentService,
         documentGenerator = documentGenerator,
-        fileServiceFacade = fileServiceFacade
+        fileServiceFacade = fileServiceFacade,
+        objectMapper = objectMapper
     )
 
     @Bean
--- configuration/src/main/kotlin/ru/alex3koval/docGenerator/configuration/di/appImplModule/AppImplModuleConfiguration.kt
+++ configuration/src/main/kotlin/ru/alex3koval/docGenerator/configuration/di/appImplModule/AppImplModuleConfiguration.kt
@@ -3,8 +3,11 @@ package ru.alex3koval.docGenerator.configuration.di.appImplModule
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import ru.alex3koval.docGenerator.configuration.di.appImplModule.client.HttpClientConfiguration
+import ru.alex3koval.docGenerator.configuration.di.appImplModule.serialization.JacksonConfiguration
 
 @Configuration
-@Import(HttpClientConfiguration::class)
-class AppImplModuleConfiguration {
-}
+@Import(
+    HttpClientConfiguration::class,
+    JacksonConfiguration::class
+)
+class AppImplModuleConfiguration
