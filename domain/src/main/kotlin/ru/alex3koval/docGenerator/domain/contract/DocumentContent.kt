02093b37afbebe7eda90059332577f8650a0eded
Alexandr Kovalenko <destructsymp@gmail.com>
2025-11-03 19:32:36 +0300

    master: rename.

--- domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/contract/DocumentContent.kt
+++ domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/contract/DocumentContent.kt
@@ -4,12 +4,9 @@ import ru.alex3koval.docGenerator.domain.vo.DocumentFormat
 import java.io.Closeable
 import java.io.InputStream
 
-abstract class Document : Closeable {
-    abstract val name: String
+abstract class DocumentContent : Closeable {
     abstract val stream: InputStream
     abstract val format: DocumentFormat
-    abstract val size: ULong
-    abstract val hash: String
 
     override fun close() = stream.close()
 }
