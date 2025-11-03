bd81989e3fb9a895cf1f72211e52d08673e8bbe5
Alexandr Kovalenko <destructsymp@gmail.com>
2025-11-03 18:38:34 +0300

    master: rename.

--- domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/contract/DocumentModifier.kt
+++ domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/contract/DocumentModifier.kt
@@ -1,5 +1,7 @@
 package ru.alex3koval.docGenerator.domain.contract
 
-interface DocumentModifier<R : Any, DTO : BaseGeneratedDocDTO> {
-    fun modify(dto: DTO, generator: DocxGenerator<DTO>)
+import ru.alex3koval.docGenerator.domain.service.generator.DocxGenerator
+
+interface DocumentModifier<DTO : BaseGeneratedDocDTO> {
+    fun modify(dto: DTO, generator: DocxGenerator)
 }
