e816f1dad4969cde25ebfe501312c4a35494f5f1
Alexandr Kovalenko <destructsymp@gmail.com>
2025-11-03 19:35:47 +0300

    master: add retry props.

--- configuration/src/main/kotlin/ru/alex3koval/docGenerator/configuration/AppEnvironment.kt
+++ configuration/src/main/kotlin/ru/alex3koval/docGenerator/configuration/AppEnvironment.kt
@@ -4,8 +4,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("app")
 data class AppEnvironment(
+    val retry: Map<String, RetryProps>,
     val webClient: Map<String, WebClientProps>
 ) {
+    data class RetryProps(
+        val maxAttempts: UShort,
+        val minDelay: ULong,
+        val jitter: Double
+    )
+
     data class WebClientProps(
         val protocol: String,
         val host: String,
