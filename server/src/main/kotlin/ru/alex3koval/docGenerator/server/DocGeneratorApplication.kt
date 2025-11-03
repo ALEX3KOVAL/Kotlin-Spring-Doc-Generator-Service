38ec3ce4f01a63d25322f95347119ac662c2750e
Alexandr Kovalenko <destructsymp@gmail.com>
2025-11-03 19:34:29 +0300

    master: nto needed.

--- server/src/main/kotlin/ru/alex3koval/docGenerator/server/DocGeneratorApplication.kt
+++ server/src/main/kotlin/ru/alex3koval/docGenerator/server/DocGeneratorApplication.kt
@@ -4,12 +4,9 @@ import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Import
 import ru.alex3koval.docGenerator.configuration.AppEnvironment
-import ru.alex3koval.docGenerator.server.configuration.MainConfiguration
 
 @SpringBootApplication
-@Import(MainConfiguration::class)
 @EnableConfigurationProperties(AppEnvironment::class)
 class DocGeneratorApplication
 
