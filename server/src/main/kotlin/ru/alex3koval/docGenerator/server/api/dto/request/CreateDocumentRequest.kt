5e9aa166be1e07bd410edeb36cfa64bdfe2095d1
Alexandr Kovalenko <destructsymp@gmail.com>
2025-11-03 19:35:01 +0300

    master: add props.

--- server/src/main/kotlin/ru/alex3koval/docGenerator/server/api/dto/request/CreateDocumentRequest.kt
+++ server/src/main/kotlin/ru/alex3koval/docGenerator/server/api/dto/request/CreateDocumentRequest.kt
@@ -3,6 +3,8 @@ package ru.alex3koval.docGenerator.server.api.dto.request
 import ru.alex3koval.docGenerator.domain.vo.DocumentFormat
 
 data class CreateDocumentRequest<TEMPLATE_ID: Any>(
+    val filename: String,
     val templateId: TEMPLATE_ID,
-    val format: DocumentFormat
+    val format: DocumentFormat,
+    val model: Map<String, Any>
 )
